92aa8c2bc566e95f45c81f7a99b141e833170385
Akash <akashadike15@gmail.com>
2024-12-30 12:11:04 +0530

    exported the custom class in a new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:helloworld/gradient_container.dart';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(body: const GradientContainer()),
     ),
   );
-}
-
-// creating a class:
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.amberAccent, Colors.redAccent],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-        ),
-      ),
-      child: const Center(
-        child: Text('Hello there!',
-            style: TextStyle(
-                fontSize: 28, color: Color.fromARGB(255, 105, 42, 42))),
-      ),
-    );
-  }
 }
