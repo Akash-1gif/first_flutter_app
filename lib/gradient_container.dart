59b695afc91c71d85a5267505897bce0625d58a2
Akash <akashadike15@gmail.com>
2025-01-07 06:16:21 +0530

    die face not flipping

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 // import 'package:helloworld/text_d.dart';
+// import 'package:helloworld/text_d.dart';
 
 //defining variables:
 
 var startColor = Alignment.topRight;
 var endColor = Alignment.bottomLeft;
+var activeDiceImage = 'images/dice-3.png';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.colors, {super.key});
+  GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
 
-  void rollDice(){
+  void rollDice() {
     // demn...
+    activeDiceImage = 'images/dice-6.png';
+    print("changing the die");
   }
 
   @override
@@ -27,12 +31,22 @@ class GradientContainer extends StatelessWidget {
         ),
         child: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               Image.asset(
-                'images/dice-4.png',
+                activeDiceImage,
                 width: 200,
               ),
-              TextButton(onPressed: rollDice, child: Text('Roll')),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                  // padding: EdgeInsets.only(top: 30),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28),
+                ),
+                child: Text('Roll'),
+              ),
             ],
           ),
         ));
